52475507caad11e196b1bebeccbb756dbd387754
mituya <atiutgh@gmail.com>
2022-11-03 22:18:47 +0900

    add:コントローラー条件変更

--- app/controllers/diagnoses_controller.rb
+++ app/controllers/diagnoses_controller.rb
@@ -1,7 +1,11 @@
 class DiagnosesController < ApplicationController
 
   def show
+  if params[:body_weight].to_i < 75
   @ball = Ball.find_by(params[:body_weight])
+  else
+  @ball = Ball.find_by(75)
+  end
   @shoes = params[:leg_size].to_i + 0.5
 
 
